32b7a493e5abc4ce58f08aab6321b3832f9386f5
Asad <asad@smartweblabs.tech>
2024-11-16 03:33:42 +0500

    env and cors updated

--- server.js
+++ server.js
@@ -7,8 +7,19 @@ const cors = require('cors');
 
 const app = express();
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || process.env.ALLOWED_ORIGINS.split(',').includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(process.env.NODE_ENV === 'production' ? corsOptions : {}));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
