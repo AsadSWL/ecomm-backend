c681527e207bab032683ab13f754dfba8a6421a1
Asad <asad@smartweblabs.tech>
2024-11-16 16:48:14 +0500

    dashboard stats

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,38 +4,37 @@ const Product = require('../models/productModel');
 const User = require('../models/userModel');
 
 exports.adminAccess = (req, res) => {
-  res.status(200).json({ message: 'Welcome Admin' });
+    res.status(200).json({ message: 'Welcome Admin' });
 };
 
 exports.branchAccess = (req, res) => {
-  res.status(200).json({ message: 'Welcome Branch' });
+    res.status(200).json({ message: 'Welcome Branch' });
 };
 
 exports.getDashboardStats = async (req, res) => {
-
     try {
         const suppliers = await Supplier.countDocuments();
-        const branches = await User.find({role: 'branch'}).countDocuments();
+        const branches = await User.find({ role: 'branch' }).countDocuments();
         const orders = await Order.countDocuments();
         const products = await Product.countDocuments();
-        const sales = await await Order.aggregate([
-          {
-              $group: {
-                  _id: null,
-                  totalSum: { $sum: "$totalPrice" }
-              }
-          }
-      ]);
+        const sales = await Order.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    totalSum: { $sum: "$totalPrice" }
+                }
+            }
+        ]);
 
         const stats = {
-          "suppliers": suppliers,
-          "branches": branches,
-          "orders": orders,
-          "products": products,
-          "sales": sales[0].totalSum,
+            "suppliers": suppliers,
+            "branches": branches,
+            "orders": orders,
+            "products": products,
+            "sales": sales[0].totalSum,
         }
 
-        res.status(201).json({ status: true, stats: stats});
+        res.status(201).json({ status: true, stats: stats });
     } catch (error) {
         res.status(500).json({ status: false, error: 'Failed to get stats' });
     }
